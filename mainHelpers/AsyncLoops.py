f6b424deba905529af2e6a50a496c7de46c16c99
Patrick Müller <patrick@plutodev.de>
2020-05-19 18:08:56 +0200

    :bug: Pushes were not sent properly
    - I dont actually know if this fixed it, I just hope so

--- mainHelpers/AsyncLoops.py
+++ mainHelpers/AsyncLoops.py
@@ -9,6 +9,10 @@ class AsnycLoops:
 	def __init__(self, main):
 		self.main = main
 		self.helpers = HelperFunctions.HelperFunctions(self.main)
+		self.sentMenuToday = False
+		self.sentLecturesToday = False
+		self.sentRatingToday = False
+		self.sentReturnToday = False
 
 	async def mainLoop(self):
 		while True:
@@ -45,32 +49,47 @@ class AsnycLoops:
 				sendPlanToday = False
 
 			# run daily at 06:00 for all users that want the menu
-			if str(timeString) == '06:00' and canteenOpen and int(datetime.now().strftime('%S')) < 30:
+			if str(timeString) == '06:00' and canteenOpen and not self.sentMenuToday:
 				logging.info('Sending menu')
+				self.sentMenuToday = True
 				self.helpers.sendMenu()
-			elif str(timeString) == '18:00' and sendPlanToday and int(datetime.now().strftime('%S')) < 30:
+			elif str(timeString) == '18:00' and sendPlanToday and not self.sentLecturesToday:
 				logging.info('Sending lecture plans')
+				self.sentLecturesToday = True
 				self.helpers.sendLectures()
-			elif str(timeString) == '14:30' and canteenOpen and int(datetime.now().strftime('%S')) < 30:
+			elif str(timeString) == '14:30' and canteenOpen and not self.sentRatingToday:
 				logging.info('Sending menu rating requests')
+				self.sentRatingToday = True
 				self.helpers.sendMenuRating()
-			elif str(timeString) == '10:00' and int(datetime.now().strftime('%S')) < 30:
+			elif str(timeString) == '10:00' and not self.sentReturnToday:
 				logging.info('Sending return directions')
+				self.sentReturnToday = True
 				self.helpers.sendReturnDirections()
 			# Reset the boolean to send the menu for today again.
 			# Do this only if seconds < 30 because otherwise it would happen twice.
-			elif timeString == '23:59' and int(datetime.now().strftime('%S')) < 30:
+			elif timeString == '23:59' and int(datetime.now().strftime('%S')) < 31:
 				logging.info('Writing usage stats')
 				self.helpers.writeUsageStats(True)
+				self.sentReturnToday = False
+				self.sentRatingToday = False
+				self.sentLecturesToday = False
+				self.sentMenuToday = False
+				# Reset all users with custom push times to receive the push the next day
+				for time in self.main.customPushTimes.keyset:
+					for pushPreference in self.main.customPushTimes[time]:
+						pushPreference[1].sentMenuPushToday = False
+						pushPreference[1].sentLecturePushToday = False
 
 			# Run all the custom push times
-			if timeString in list(self.main.customPushTimes) and int(datetime.now().strftime('%S')) >= 30:
+			if timeString in list(self.main.customPushTimes):
 				for pushPreference in self.main.customPushTimes[timeString]:
-					if pushPreference[0] == 'menu':
+					if pushPreference[0] == 'menu' and not pushPreference[1].sentMenuPushToday:
 						logging.info('Sending custom menu push')
+						pushPreference[1].sentMenuPushToday = True
 						self.helpers.sendMenu(True, pushPreference[1])
-					elif pushPreference[0] == 'lecture':
+					elif pushPreference[0] == 'lecture' and not pushPreference[1].sentLecturePushToday:
 						logging.info('Sending custom lecture push')
+						pushPreference[1].sentLecturePushToday = True
 						self.helpers.sendLectures(True, pushPreference[1])
 					else:
 						logging.warning('AsyncLoops.pushLoop(): Tried to send custom push with unknown type %s',
